dda049fbe89538e3f5b843b2d182f7aba7be9f80
allan.le-lay <allan.lelay@gmail.com>
2022-07-18 15:23:19 +0200

    5.2 finti

--- spiruline/src/Supplier.js
+++ spiruline/src/Supplier.js
@@ -3,14 +3,8 @@ import Moment from 'moment';
 import Toto from './leaf.png'
 
 
-function Supplier() {
-    // Déclare une nouvelle variable d'état, que nous appellerons « count »
-    const [data] = React.useState({
-        name: 'Mon fournisseur',
-        status: false, // est ce qu'il y a du stock
-        checkedAt: new Date() // date de la dernière mise à jour du stock
-    })
-    const formatedDate =  Moment().format('DD-MM-YYYY');
+function Supplier(props) {
+     const formatedDate =  Moment(props.checkedAt).format('DD-MM-YYYY');
     return(
         <div>
             <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
@@ -18,8 +12,8 @@ function Supplier() {
                     <img className="h-12 w-12" src={ Toto } alt="ChitChat Logo"/>
                 </div>
                 <div>
-                    <div className="text-xl font-medium text-black">{ data.name }</div>
-                    <p className="text-slate-500">En stock : <span class = {  data.status ? 'text-green-400' : 'text-red-800' }>{ data.status ? 'Oui' : 'Non' }</span></p>
+                    <div className="text-xl font-medium text-black">{ props.name }</div>
+                    <p className="text-slate-500">En stock : <span class = {  props.status ? 'text-green-400' : 'text-red-800' }>{ props.status ? 'Oui' : 'Non' }</span></p>
                     <p className="text-slate-500">MàJ : { formatedDate }</p>
                 </div>
             </div>
--- spiruline/src/SuppliersList.js
+++ spiruline/src/SuppliersList.js
@@ -1,12 +1,19 @@
 import Nav from "./Nav";
 import Supplier from "./Supplier";
+import React, { useState } from 'react';
 
 function SuppliersList() {
+    const [data] = React.useState({
+        name: 'Oxford',
+        status: true,
+        checkedAt: new Date()
+    })
     return (
         <div>
             <Nav/>
             <h1>Liste des fournisseurs</h1>
-            <Supplier></Supplier>
+            <Supplier name={ data.name } status={ data.status } checkedAt = { data.checkedAt }>
+            </Supplier>
         </div>
     )
 }
